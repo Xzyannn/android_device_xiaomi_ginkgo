f8246eb9dd45f6ad36eb2d0ec0ce6a9141fc4be8
Muhammad Fitriyan <72380503+Xzyannn@users.noreply.github.com>
2021-10-17 17:18:44 +0700

    bringup evox

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/evox_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    evox_ginkgo-user \
+    evox_ginkgo-userdebug \
+    evox_ginkgo-eng
